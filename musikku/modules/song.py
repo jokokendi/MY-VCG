ed59518913537310c7af8a86991f9b816f8025b0
jokokendi <80366713+jokokendi@users.noreply.github.com>
2021-06-03 15:42:55 +0700

    Update song.py

--- musikku/modules/song.py
+++ musikku/modules/song.py
@@ -31,7 +31,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += " " + str(i)
     print(query)
-    m = message.reply("🔎 Mencari lag...")
+    m = message.reply("🔎 Mencari lagu...")
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -57,7 +57,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎵 Uploaded by @VCGSupportGroup**"
+        rep = "**🎵 Uploaded by MY-VCG**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
